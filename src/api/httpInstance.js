dfe6b56ffcf180970430c740e8a31f696e8f2cac
Chenxi-Liu <329052927@qq.com>
2021-09-09 15:01:35 +0800

    feat: axios

---
+++ src/api/httpInstance.js
@@ -0,0 +1,80 @@
+/*
+ * @Author: 刘晨曦
+ * @Date: 2021-09-09 14:58:54
+ * @LastEditTime: 2021-09-09 15:00:43
+ * @LastEditors: Please set LastEditors
+ * @Description: Axios的封装
+ * @FilePath: \widget-vue2\src\api\httpInstance.js
+ */
+import axios from 'axios'
+import { message as Message } from 'antd'
+
+const REQUEST_SUCCESS = 0
+
+const http = axios.create({
+  timeout: 20000,
+  withCredentials: true,
+  headers: { 'X-Requested-With': 'XMLHttpRequest' },
+  baseURL: ''
+})
+
+// 响应拦截器
+http.interceptors.response.use(function (response) {
+  // 请求多语言的json文件
+  if (/.*\.json$/.test(response.config.url)) {
+    return response
+  }
+
+  // 对错误进行统一处理
+  if (response.data.code !== REQUEST_SUCCESS) {
+    if (!response.config.noMsg && response.data.msg) {
+      Message.error(response.data.msg)
+    }
+    return Promise.reject(response)
+  } else if (
+    response.data.code === REQUEST_SUCCESS &&
+    response.config.successNotify &&
+    response.config.successMsg
+  ) {
+    // 弹出成功提示
+    Message.success(response.config.successMsg)
+  }
+  return Promise.resolve({
+    code: response.data.code,
+    msg: response.data.msg,
+    data: response.data.data
+  })
+}, function (error) {
+  switch (error.response.status) {
+    case 401:
+      Message.error('暂无权限访问，请先登录!')
+      break
+    case 403:
+      Message.error('登录过期，请重新登录!')
+      break
+    case 404:
+      Message.error('访问资源不存在!')
+      break
+    case 500:
+      Message.error('服务器内部错误!')
+      break
+    default:
+      if (Object.prototype.toString.call(error.response.data) === '[object Object]') {
+        Message.error(error.response.data)
+      }
+  }
+  return Promise.reject(error)
+})
+
+// 请求拦截器
+http.interceptors.request.use((config) => {
+  if (config.method === 'get') {
+    config.params = { ...config.params, _t: Date.now() }
+  }
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+export default http
---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-09 14:58:32
+ * @LastEditTime: 2021-09-09 14:58:57
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \widget-vue2\src\api\index.js
+ */
+import http from '@/core/httpInstance'
+
+function getUserInfo () {
+  return http({
+    method: 'get',
+    successNotify: true,
+    successMsg: '获取成功',
+    url: 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg?_=1630720260336&cv=4747474&ct=24&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=1&uin=0&g_tk_new_20200303=5381&g_tk=5381&hostUin=0'
+  })
+}
+
+export {
+  getUserInfo
+}
