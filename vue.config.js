eede17edcab886f92bb4b4eab2063939f1365768
Chenxi-Liu <329052927@qq.com>
2021-09-09 15:42:43 +0800

    update: devServer config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 刘晨曦
  * @Date: 2021-09-09 11:25:38
- * @LastEditTime: 2021-09-09 14:52:10
+ * @LastEditTime: 2021-09-09 15:41:04
  * @LastEditors: Please set LastEditors
  * @Description: VUE的配置
  * @FilePath: \widget-vue2\vue.config.js
@@ -10,6 +10,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: `/${process.env.VUE_APP_CONTEXT}`,
+  assetsDir: process.env.VUE_APP_ASSETS,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', path.join(__dirname, 'src'))
@@ -19,6 +20,16 @@ module.exports = {
     loaderOptions: {
       scss: {
         prependData: `@import '@/styles/index.scss';` // 全局样式
+      }
+    }
+  },
+
+  // 用于生产模式下前后端联调
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
       }
     }
   }
