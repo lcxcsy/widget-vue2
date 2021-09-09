d0fdfd964cfca468036270e73f5a21244b05f813
Chenxi-Liu <329052927@qq.com>
2021-09-09 14:53:09 +0800

    feat: add sass sass-loader

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 刘晨曦
  * @Date: 2021-09-09 11:25:38
- * @LastEditTime: 2021-09-09 14:03:14
+ * @LastEditTime: 2021-09-09 14:52:10
  * @LastEditors: Please set LastEditors
  * @Description: VUE的配置
  * @FilePath: \widget-vue2\vue.config.js
@@ -14,5 +14,12 @@ module.exports = {
     config.resolve.alias
       .set('@', path.join(__dirname, 'src'))
       .set('public', path.join(__dirname, 'public'))
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import '@/styles/index.scss';` // 全局样式
+      }
+    }
   }
 }
