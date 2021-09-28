b91ea7b84f8f2f0bfd007b369d7b5f020e00847d
Chenxi-Liu <329052927@qq.com>
2021-09-28 11:30:35 +0800

    update: dist

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 刘晨曦
  * @Date: 2021-09-09 11:25:38
- * @LastEditTime: 2021-09-10 11:35:11
+ * @LastEditTime: 2021-09-28 11:24:48
  * @LastEditors: Please set LastEditors
  * @Description: VUE的配置
  * @FilePath: \widget-vue2\vue.config.js
@@ -11,6 +11,8 @@ const path = require('path')
 module.exports = {
   publicPath: `/${process.env.VUE_APP_CONTEXT}`,
   assetsDir: process.env.VUE_APP_ASSETS,
+  // 生产环境下关闭SourceMap
+  productionSourceMap:false,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', path.join(__dirname, 'src'))
